226e8768e3fc4c04b62e7fcff3e040ed928a77e1
abhay-gupta-199 <abhaygupta1915@gmail.com>
2025-07-13 00:19:10 +0530

    A-1 done

--- assignment-1.py
+++ assignment-1.py
@@ -1,4 +1,4 @@
-# 1
+# Q.1.
 
 class DivisionByZeroError(Exception):
     """Exception thrown when attempting to divide by zero."""
@@ -26,3 +26,39 @@ except DivisionByZeroError as err:
     print(f"Error: {err}")
 
     # Output => Error: Division by zero is not allowed.
+
+# Q.2.
+
+# To save 2D data into a CSV text file as
+
+# A, B, C
+# 5, 10, 15
+# 6, 11, 16
+# 7, 12, 17
+
+
+data = [['A', 'B', 'C'],
+        [5, 10, 15],
+        [6, 11, 16],
+        [7, 12, 17]]
+
+# Write data to file
+with open("matrix.csv", 'w') as f:
+    for row in data:
+        for val in row:
+            if row.index(val) == len(row) - 1:
+                f.write(str(val))
+            else:
+                f.write(str(val) + ', ')
+        f.write('\n')
+
+# Read and display file
+with open("matrix.csv", 'r') as f:
+    for line in f:
+        print(line, end='')
+
+# Output:
+# A, B, C
+# 5, 10, 15
+# 6, 11, 16
+# 7, 12, 17
